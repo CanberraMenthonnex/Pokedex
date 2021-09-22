a2c150df2547cd8cc6ca05bddabf23b96b48cea7
Mathieu <mathieu.raimbault@hotmail.com>
2021-09-22 11:11:03 +0100

    :sparkles: link pokemon data with pokedex

--- src/components/pokemonItem.js
+++ src/components/pokemonItem.js
@@ -1,14 +1,15 @@
 import { PokemonType } from "./pokemonType";
+import { fetchPokemonImage } from "../services/pokemonImageService";
 
-export function PokemonItem({name, types}) {
+export function PokemonItem({name, types, id}) {
     return (
         <article className="pokemon-item">
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png" />
+            <img src={fetchPokemonImage(id)} />
             <h3>{name}</h3>
             <footer>
                 {
-                    types.map(type => (
-                        <PokemonType type={type.name} />
+                    types.map((type, index) => (
+                        <PokemonType key={index} type={type.name} />
                     ))
                 }
             </footer>
--- src/components/pokemonList.js
+++ src/components/pokemonList.js
@@ -1,132 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { PokemonItem } from "./pokemonItem"
+import { getListPokemon, getDataPokemon } from "../services";
 
 export function PokemonList() {
 
-    const [pokemons, setPokemons] = useState([
-        {
-            name: "bulbasaur",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/1/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "ivysaur",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/2/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "venusaur",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/3/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "charmander",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/4/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "charmeleon",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/5/",
-            types: [{name: "fire"}]
-        },
-        {
-            id:11,
-            name: "charizard",
-            url: "https://pokeapi.co/api/v2/pokemon/6/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "squirtle",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/7/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "wartortle",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/8/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "blastoise",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/9/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "caterpie",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/10/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "metapod",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/11/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "butterfree",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/12/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "weedle",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/13/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "kakuna",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/14/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "beedrill",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/15/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "pidgey",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/16/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "pidgeotto",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/17/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "pidgeot",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/18/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "rattata",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/19/",
-            types: [{name: "fire"}]
-        },
-        {
-            name: "raticate",
-            id: 10,
-            url: "https://pokeapi.co/api/v2/pokemon/20/",
-            types: [{name: "fire"}]
-        }
-    ])
+    const [pokemons, setPokemons] = useState([]);
+    const [index, setIndex] = useState(0)
 
-    const [index, setIndex] = useState(1)
+    useEffect(() => {
+        if(index < pokemons.length - 1) {
+            return 
+        }
+        getListPokemon(0,10).then((response) => {            
+            return Promise.all(response.data.results.map(pokemon => getDataPokemon(pokemon.url).then(data => data.data)))            
+        })
+        .then(pokemons => setPokemons(prev => [...prev, ...pokemons]));
+    }, [index]);
+
+   
 
     const prevItem = () => {
         setIndex(index => index > 0 ? index - 1 : 0)
@@ -134,11 +25,7 @@ export function PokemonList() {
 
     const nextItem = () => {
         setIndex(index => {
-            const newIndex = index + 1 
-            if(newIndex > pokemons.length - 1) {
-                //fetch new pokemons
-            }
-            return newIndex
+            return index + 1
         })
     }
 
@@ -148,8 +35,8 @@ export function PokemonList() {
             <div className="pokedex-screen">
                 <div className="pokemon-list" style={{transform: `translateX(-${index * 30}vw)`}}>
                     {
-                        pokemons.map(pokemon => (
-                            <PokemonItem name={pokemon.name} types={pokemon.types} />
+                        pokemons.map((pokemon) => (
+                            <PokemonItem id={pokemon.id} key={pokemon.id} name={pokemon.name} types={pokemon.types} />
                         ))
                     }
                 </div>
@@ -157,7 +44,7 @@ export function PokemonList() {
             <div className="pokedex-footer">
                 <div className="pokedex-id">
                     {
-                       pokemons ? pokemons[index].id : ""
+                       pokemons && pokemons.length > 0 ? pokemons[index].id : ""
                     }
                 </div>
                 <div className="pokedex-button">
